59540e5d2ed1c91c3476abc4a604795d3e4231cb
Helfstein <Helfstein-Note>
2019-02-19 11:48:12 -0300

    custom propertyconverter added

--- src/ORest/ORestPropListConverter.cs
+++ src/ORest/ORestPropListConverter.cs
@@ -6,28 +6,36 @@ namespace ORest {
     public class ORestPropListConverter : JsonConverter {
 
         public string Path { get;}
+        public bool RegisterErrors { get;}
         
         public ORestPropListConverter(string path) {
             Path = path;
         }
-        
+
+        public ORestPropListConverter(string path, bool registerErrors) : this(path) {
+            RegisterErrors = registerErrors;
+        }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
             // throw new NotImplementedException();
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
-
-            var jo = JObject.Load(reader);
-            //var targetObj = Activator.CreateInstance(objectType);
-            //var isList = objectType.DeclaringType == typeof(IEnumerable<>);
-            //var jsonPath = !string.IsNullOrWhiteSpace(Path) ? Path : prop.Name;
-
-            var token = jo.SelectToken(Path);
-
-            if (token != null && token.Type != JTokenType.Null) {
-                var value = token.ToObject(objectType, serializer);
-                return value;
+            try {
+                var jo = JObject.Load(reader);
+                //var targetObj = Activator.CreateInstance(objectType);
+                //var isList = objectType.DeclaringType == typeof(IEnumerable<>);
+                //var jsonPath = !string.IsNullOrWhiteSpace(Path) ? Path : prop.Name;
+                var token = jo.SelectToken(Path);
+                if (token != null && token.Type != JTokenType.Null) {
+                    var value = token.ToObject(objectType, serializer);
+                    return value;
+                }
+            }
+            catch (Exception e) {
+                if (RegisterErrors) {
+                    Console.WriteLine(e);
+                }
             }
 
             return existingValue;
